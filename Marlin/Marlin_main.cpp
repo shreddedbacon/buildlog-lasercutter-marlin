9fef6bca4a1117af6562139c77e4e1475fdc3df5
Ben Jackson <bacon@shreddedbacon.com>
2017-01-01 16:51:14 +1100

    changes to try fix raster, new boot image

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -526,7 +526,14 @@ void get_command()
       if(!comment_mode){
         comment_mode = false; //for new command
         fromsd[bufindw] = false;
-        if(strchr(cmdbuffer[bufindw], 'N') != NULL)
+
+        //Turnkey - Changed <=6 to <=8 to allow up to 999999 lines of raster data to be transmitted per raster node
+        //This area needs improving for stability.
+        if(  (strstr_P(cmdbuffer[bufindw], PSTR("G7")) == NULL && strchr(cmdbuffer[bufindw], 'N') != NULL) || //For non G7 commands, run as normal.
+            ((strstr_P(cmdbuffer[bufindw], PSTR("G7")) != NULL) &&
+            ((strstr_P(cmdbuffer[bufindw], PSTR("G7")) - strchr(cmdbuffer[bufindw], 'N')  <=8 ) &&
+            (strstr_P(cmdbuffer[bufindw], PSTR("G7")) - strchr(cmdbuffer[bufindw], 'N')  >0 )))
+          )
         {
           strchr_pointer = strchr(cmdbuffer[bufindw], 'N');
           gcode_N = (strtol(&cmdbuffer[bufindw][strchr_pointer - cmdbuffer[bufindw] + 1], NULL, 10));
@@ -792,6 +799,10 @@ static void homeaxis(int axis) {
     has_axis_homed[axis] = true;
     current_position[axis] = 0;
     #ifdef MUVE_Z_PEEL
+    if (axis == Z_AXIS) {		
+        has_axis_homed[E_AXIS] = true;		
+        current_position[E_AXIS] = 0;		
+      }
       plan_set_position(current_position[X_AXIS], current_position[Y_AXIS], current_position[Z_AXIS], current_position[Z_AXIS]);
       destination[axis] = 1.5 * max_length(axis) * axis_home_dir;
       feedrate = homing_feedrate[axis];
@@ -1063,7 +1074,8 @@ void process_commands()
     #ifdef LASER_RASTER
     case 7: //G7 Execute raster line
       if (code_seen('L')) laser.raster_raw_length = int(code_value());
-	  if (code_seen('N')) {
+	 // if (code_seen('N')) {
+         if (code_seen('$')) {
 		laser.raster_direction = (bool)code_value();
 		destination[Y_AXIS] = current_position[Y_AXIS] + (laser.raster_mm_per_pulse * laser.raster_aspect_ratio); // increment Y axis
 	  }
@@ -1082,7 +1094,8 @@ void process_commands()
         }
 	  }
 	  laser.ppm = 1 / laser.raster_mm_per_pulse;
-	  laser.duration = labs(1 / (feedrate * laser.ppm) * 1000000);
+	  //laser.duration = labs(1 / (feedrate * laser.ppm) * 1000000);
+          laser.duration = (1000000 / ( feedrate / 60)) / laser.ppm; // (1 second in microseconds / (time to move 1mm in microseconds)) / (pulses per mm) = Duration of pulse, taking into account feedrate as speed and ppm
 	  laser.mode = RASTER;
 	  laser.status = LASER_ON;
 	  laser.fired = RASTER;
@@ -1106,7 +1119,7 @@ void process_commands()
       }
 
       break;
-    case 11: // G10 retract_recover
+    case 11: // G11 retract_recover
       if(!retracted)
       {
         destination[X_AXIS]=current_position[X_AXIS];
@@ -1347,10 +1360,12 @@ void process_commands()
 
       laser.status = LASER_ON;
       laser.fired = LASER_FIRE_SPINDLE;
+      lcd_update();
       prepare_move();
       break;
     case 5:  //M5 stop firing laser
 	  laser.status = LASER_OFF;
+          lcd_update();
 	  prepare_move();
       break;
 #endif // LASER_FIRE_SPINDLE
@@ -2409,11 +2424,20 @@ void process_commands()
 	#ifdef LASER
 	case 649: // M649 set laser options
 	{
-	  if (code_seen('S') && !IsStopped()) laser.intensity = (float) code_value();
+	  if (code_seen('S') && !IsStopped()) {
+          laser.intensity = (float) code_value();
+          laser.rasterlaserpower =  laser.intensity;
+      }
       if (code_seen('L') && !IsStopped()) laser.duration = (unsigned long) labs(code_value());
       if (code_seen('P') && !IsStopped()) laser.ppm = (float) code_value();
       if (code_seen('D') && !IsStopped()) laser.diagnostics = (bool) code_value();
       if (code_seen('B') && !IsStopped()) laser_set_mode((int) code_value());
+      if (code_seen('R') && !IsStopped()) laser.raster_mm_per_pulse = ((float) code_value());
+      if (code_seen('F')) {
+        next_feedrate = code_value();
+        if(next_feedrate > 0.0) feedrate = next_feedrate;
+      }
+      
 	}
 	break;
 	#endif // LASER
